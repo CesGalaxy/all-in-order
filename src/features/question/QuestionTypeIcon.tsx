c8f8bde42e6642b879986ffa1a30383a55f3a867
CesGalaxy <cesgalaxy@outlook.com>
2024-09-28 12:19:40 +0200

    Remove old topic tests & questions

--- src/features/question/QuestionTypeIcon.tsx
+++
@@ -1,8 +0,0 @@
-import { IconListDetails, IconWriting } from "@tabler/icons-react";
-
-const QUESTION_TYPE_ICONS = {
-    "choice": <IconListDetails />,
-    "fill_the_gap": <IconWriting />
-};
-
-export default QUESTION_TYPE_ICONS;
