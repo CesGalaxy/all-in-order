f214a709c86eeaa09d46494209a4359b710e22b9
CesGalaxy <cesgalaxy@outlook.com>
2024-10-05 18:06:47 +0200

    Fix exam review button before submission

--- src/app/practices/[practiceId]/start/_feature/BottomNavigation.tsx
+++ src/app/practices/[practiceId]/start/_feature/BottomNavigation.tsx
@@ -14,7 +14,7 @@ import {
 import { Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/modal";
 import { Chip } from "@nextui-org/chip";
 import { validateQuestion } from "@/features/beta_question";
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
 import SubmitExamButton from "@/app/practices/[practiceId]/start/_feature/SubmitExamButton";
 
 export default function BottomNavigation() {
@@ -37,13 +37,52 @@ export default function BottomNavigation() {
         console.log(correct);
     }, [answerDraft, data, updateCurrentActivity]);
 
+    const modal = useMemo(() => <Modal isOpen={isOpen} onClose={onOpenChange}>
+        <ModalContent className="overflow-y-auto max-h-full">
+            {(onClose) => <>
+                <ModalHeader className="flex flex-col gap-1">Practice activities</ModalHeader>
+                <ModalBody as="ul" className="grid">
+                    {activities.map((activity, i) => <Button
+                        key={activity.id}
+                        isDisabled={
+                            !(activity.answer || activity.answerDraft)
+                            &&
+                            !(activities.findLastIndex(activity => activity.answer || activity.answerDraft) + 1 >= i)
+                        }
+                        color={activity.correct === true ? "success" : activity.correct === false ? "danger" : "default"}
+                        onPress={() => {
+                            setCurrentActivityIndex(i);
+                            onClose();
+                        }}
+                    >
+                        {activity.data.title}
+                    </Button>)}
+                </ModalBody>
+                <ModalFooter className="justify-start">
+                    <Chip color="success" startContent={<IconCheck/>}>
+                        {activities.filter(activity => activity.correct).length}
+                    </Chip>
+                    <Chip color="danger" startContent={<IconX/>}>
+                        {activities.filter(activity => activity.correct === false).length}
+                    </Chip>
+                    <Chip startContent={<IconQuestionMark/>}>
+                        {activities.filter(activity => activity.correct === undefined).length}
+                    </Chip>
+                </ModalFooter>
+            </>}
+        </ModalContent>
+    </Modal>, [activities, isOpen, onOpenChange, setCurrentActivityIndex]);
+
     // If all activities have been answered, show the submit button
-    if (activities.every(activity => activity.answer)) return <ButtonGroup className="w-full">
-        <Button isIconOnly onPress={onOpen}>
-            <IconListCheck/>
-        </Button>
-        <SubmitExamButton/>
-    </ButtonGroup>;
+    if (activities.every(activity => activity.answer)) return <>
+        <ButtonGroup className="w-full">
+            <Button isIconOnly onPress={onOpen}>
+                <IconListCheck/>
+            </Button>
+            <SubmitExamButton/>
+        </ButtonGroup>
+        {modal}
+    </>;
 
     const statusColor = answer
         ? correct
@@ -77,40 +116,6 @@ export default function BottomNavigation() {
                 <IconChevronRight/>
             </Button>
         </ButtonGroup>
-        <Modal isOpen={isOpen} onClose={onOpenChange}>
-            <ModalContent className="overflow-y-auto max-h-full">
-                {(onClose) => <>
-                    <ModalHeader className="flex flex-col gap-1">Practice activities</ModalHeader>
-                    <ModalBody as="ul" className="grid">
-                        {activities.map((activity, i) => <Button
-                            key={activity.id}
-                            isDisabled={
-                                !(activity.answer || activity.answerDraft)
-                                &&
-                                !(activities.findLastIndex(activity => activity.answer || activity.answerDraft) + 1 >= i)
-                            }
-                            color={activity.correct === true ? "success" : activity.correct === false ? "danger" : "default"}
-                            onPress={() => {
-                                setCurrentActivityIndex(i);
-                                onClose();
-                            }}
-                        >
-                            {activity.data.title}
-                        </Button>)}
-                    </ModalBody>
-                    <ModalFooter className="justify-start">
-                        <Chip color="success" startContent={<IconCheck/>}>
-                            {activities.filter(activity => activity.correct).length}
-                        </Chip>
-                        <Chip color="danger" startContent={<IconX/>}>
-                            {activities.filter(activity => activity.correct === false).length}
-                        </Chip>
-                        <Chip startContent={<IconQuestionMark/>}>
-                            {activities.filter(activity => activity.correct === undefined).length}
-                        </Chip>
-                    </ModalFooter>
-                </>}
-            </ModalContent>
-        </Modal>
+        {modal}
     </>
 }
