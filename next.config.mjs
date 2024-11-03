4e39aa96e25efca4c84203b3e22ca7d2a6ff4b4a
CesGalaxy <cesgalaxy@outlook.com>
2024-11-03 10:50:54 +0100

    Install Next.js bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 import createNextIntlPlugin from 'next-intl/plugin';
+import createNextBundleAnalyzer from '@next/bundle-analyzer';
 
 const withNextIntl = createNextIntlPlugin();
+const withBundleAnalyzer = createNextBundleAnalyzer({
+    enabled: process.env.ANALYZE === 'true',
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     trailingSlash: true,
     experimental: {
-        optimizePackageImports: ["@tabler/icons-react"],
+        // optimizePackageImports: ["@tabler/icons-react"],
     }
 };
 
-export default withNextIntl(nextConfig);
+export default withBundleAnalyzer(withNextIntl(nextConfig));
