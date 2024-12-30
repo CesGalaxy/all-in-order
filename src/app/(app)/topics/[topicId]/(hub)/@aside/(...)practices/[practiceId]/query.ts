c29ede2f2901991d080f4312ef62a96c15114b3b
CesGalaxy <cesgalaxy@outlook.com>
2024-12-30 11:54:23 +0100

    Add error logs to topic aside query

--- src/app/(app)/topics/[topicId]/(hub)/@aside/(...)practices/[practiceId]/query.ts
+++ src/app/(app)/topics/[topicId]/(hub)/@aside/(...)practices/[practiceId]/query.ts
@@ -5,10 +5,14 @@ import { getMyProfile } from "@/supabase/auth/profile";
 export const getTopicAsidePractice = cache(async (practiceId: string | number) => {
     const profile = await getMyProfile();
     const supabaseClient = await getSupabase();
-    return supabaseClient
-        .from("practices")
-        .select("id, title, topic_id, activities:topic_activities(*), attempts:practice_attempts(*)")
-        .eq("id", Number(practiceId))
-        .eq("attempts.profile_id", profile.id)
-        .maybeSingle();
+    try {
+        return await supabaseClient
+            .from("practices")
+            .select("id, title, topic_id, activities:topic_activities(*), attempts:practice_attempts(*)")
+            .eq("id", Number(practiceId))
+            .eq("attempts.profile_id", profile.id)
+            .maybeSingle();
+    } catch (e) {
+        console.error("CATCH: ", e);
+    }
 });
