c63eb04d572fc18dbb0e26a945831bdad7d0fb8f
CesGalaxy <cesgalaxy@outlook.com>
2025-01-11 23:35:20 +0100

    Show Notion pages at notebook

--- src/app/(app)/topics/[topicId]/notebook/notion/page.tsx
+++ src/app/(app)/topics/[topicId]/notebook/notion/page.tsx
@@ -1,51 +1,52 @@
 import { getUser } from "@/supabase/auth/user";
-import getSupabase from "@/supabase/server";
+import { createSupabaseServerClient } from "@/supabase/server";
 import { redirect } from "next/navigation";
-import ErrorView from "@/components/views/ErrorView";
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { revalidatePath } from "next/cache";
+import { Client } from "@notionhq/client";
+
+const NOTION_API_URL = "https://api.notion.com/v1/oauth/authorize?client_id=178d872b-594c-8089-9682-00378b143de0";
 
 export default async function Page({ params }: { params: Promise<{ topicId: string }>, }) {
     const { topicId } = await params;
     const user = await getUser();
 
-    const hasNotionIdentity = user.identities?.some(identity => identity.provider === "notion");
+    const cookieStore = await cookies();
+    const notionToken = cookieStore.get("notion_token")?.value;
 
-    if (!hasNotionIdentity) {
+    if (!notionToken) {
         const notebookPath = `/topics/${topicId}/notebook`;
 
         // Get the domain or (error) redirect to the notebook page
         const headersList = await headers();
         const host = headersList.get("host") || redirect(notebookPath);
+        const hostAndProtocol = (host.includes("localhost:") ? "http://" : "https://") + host;
 
-        const redirectTo = (host.includes("localhost:") ? "http://" : "https://") + host + notebookPath + "/notion/callback";
+        const state = encodeURIComponent(hostAndProtocol + notebookPath + "/notion");
+        const redirectTo = encodeURIComponent(hostAndProtocol + "/api/notion/callback");
 
         // Link the Notion identity
-        const supabaseClient = await getSupabase();
-        const { data, error } = await supabaseClient.auth.linkIdentity({
-            provider: "notion",
-            options: {
-                queryParams: {
-                    // state: `notebook-${topicId}`,
-                },
-                redirectTo,
-            },
-        })
-
-        if (error) return <ErrorView message={error.message}/>
-        redirect(data.url);
+        redirect(`${NOTION_API_URL}&response_type=code&owner=user&redirect_uri=${redirectTo}&state=${state}`);
     }
+
+    const notion = new Client({ auth: notionToken });
+    const { results: pages } = await notion.search({
+        filter: {
+            property: "object",
+            value: "page",
+        },
+    })
 
     return <div>
         <button onClick={async () => {
             "use server";
-            const supabaseClient = await getSupabase();
+            const supabaseClient = await createSupabaseServerClient();
             const { data } = await supabaseClient.auth.getUserIdentities();
             const notionIdentity = data!.identities.find(identity => identity.provider === "notion");
             if (notionIdentity) await supabaseClient.auth.unlinkIdentity(notionIdentity);
             revalidatePath("/", "layout");
         }}>Unlink Notion
         </button>
-        <pre>{JSON.stringify(user, null, 2)}</pre>
+        <pre>{JSON.stringify({ pages, user }, null, 2)}</pre>
     </div>
 }
