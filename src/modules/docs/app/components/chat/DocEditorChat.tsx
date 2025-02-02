b3aaf0fb2d8beecff8682b74fdc0f713519277fe
CesGalaxy <cesgalaxy@outlook.com>
2025-02-02 19:43:15 +0100

    Add char count to document AI chat

--- src/modules/docs/app/components/chat/DocEditorChat.tsx
+++ src/modules/docs/app/components/chat/DocEditorChat.tsx
@@ -8,13 +8,38 @@ import { Alert } from "@heroui/alert";
 import { usePathname } from "next/navigation";
 import { Tooltip } from "@heroui/tooltip";
 import { Popover, PopoverContent, PopoverTrigger } from "@heroui/popover";
+import React, { useRef } from "react";
+import { ScrollShadow } from "@heroui/scroll-shadow";
+
+const IDEAS = [
+    {
+        title: "Summarize",
+        description: "the doc",
+    },
+    {
+        title: "Generate questions",
+        description: "for learning the content",
+    },
+    {
+        title: "Explain it",
+        description: "as if I was a child",
+    },
+    {
+        title: "Make an index",
+        description: "of all the contents",
+    },
+]
 
 function DocEditorChat() {
     const pathname = usePathname();
-    const { messages, input, handleInputChange, handleSubmit, error } = useChat({
+    const { messages, input, handleInputChange, setInput, handleSubmit, error } = useChat({
         api: pathname.replace("edit", "api/chat"),
         onError: (error) => console.error(error),
     });
+
+    const remainingChars = Math.max(200 - input.length, 0);
+
+    const formRef = useRef<HTMLFormElement>(null);
 
     return <div
         className="w-full lg:w-96 shrink-0 flex flex-col gap-2 p-2 lg:sticky top-0 lg:flex-col justify-between items-center lg:h-dvh z-50 bg-background">
@@ -57,16 +82,29 @@ function DocEditorChat() {
                 </div>
             ))}
         </div>
-        <form className="w-full" onSubmit={handleSubmit}>
+        <form className="w-full" onSubmit={handleSubmit} ref={formRef}>
             <Textarea
                 value={input}
                 onChange={handleInputChange}
                 placeholder="Type your message here"
                 className="w-full"
-                endContent={<Button isIconOnly color="primary" type="submit"><IconSend/></Button>}
+                endContent={<div className="w-min flex flex-col items-center">
+                    <Button isIconOnly color="primary" type="submit"
+                            isDisabled={remainingChars < 1}><IconSend/></Button>
+                    <span className={remainingChars > 20 ? "text-default-400" : "text-danger"}>{remainingChars}</span>
+                </div>}
                 name="prompt"
             />
         </form>
+        <ScrollShadow orientation="horizontal" hideScrollBar as={"ul"} className="flex items-center w-full gap-2">
+            {IDEAS.map(({ title, description }, i) => <li key={i}>
+                <Button className="flex h-14 flex-col items-start gap-0" variant="flat"
+                        onPress={() => setInput(title + " " + description)}>
+                    <p>{title}</p>
+                    <p className="text-default-500">{description}</p>
+                </Button>
+            </li>)}
+        </ScrollShadow>
         {error && <Alert color="danger" title={error.name} isClosable className="max-h-fit">{error.message}</Alert>}
     </div>
 }
